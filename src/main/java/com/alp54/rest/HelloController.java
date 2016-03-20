6153849687a3d444fcd4396aae4a3ec3e37acfda
Thomas Geiger <thomas.geiger@anivo.ch>
2016-03-20 21:11:10 +0100

    serve static content as well

--- src/main/java/com/alp54/rest/HelloController.java
+++ src/main/java/com/alp54/rest/HelloController.java
@@ -1,19 +1,19 @@
 package com.alp54.rest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.alp54.domain.Hello;
 
-@RestController("/hello")
+@Controller
 public class HelloController {
     
     private static long id = 0;
 
-    @RequestMapping(method=RequestMethod.GET)
+    @RequestMapping(path="/hello", method=RequestMethod.GET)
     public @ResponseBody Hello getHello(@RequestParam(value="message", required=false, defaultValue="hello world!") String message) {
         return new Hello(id++, message);
     }
